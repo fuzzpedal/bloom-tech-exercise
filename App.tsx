e8877a72ea9a50a264f379eb07b3c2cbe0944771
Matt Hall <reply@matthall.tv>
2024-01-23 21:31:16 +0000

    initial commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>Bloom Text Exercise</Text>
+    </SafeAreaView>
+  );
+}
