9006b3889bd5fa11e5f1be64df8592376181fd51
Matt Hall <reply@matthall.tv>
2024-01-24 13:11:28 +0000

    only has border on current day

--- src/components/Streak/Streak.view.tsx
+++ src/components/Streak/Streak.view.tsx
@@ -20,7 +20,7 @@ interface Props {
 
 export const StreakView: FC<Props> = ({ days }) => {
   const r = 20;
-  const spacing = 15;
+  const spacing = 14;
   const w = r * 2 + spacing;
 
   const strokeWidth = 3;
@@ -46,6 +46,9 @@ export const StreakView: FC<Props> = ({ days }) => {
           const ty = fontSize;
           const cy = r + fontSize + strokeWidth + 10;
 
+          const isCurrentDay = days.findLastIndex(item => item.selected) === i;
+          const fullStreak = !days.map(item => item.selected).includes(false);
+
           return (
             <Group key={day.fullName}>
               <Group>
@@ -61,30 +64,47 @@ export const StreakView: FC<Props> = ({ days }) => {
               <Group>
                 {day.selected ? (
                   <>
-                    <Circle cx={cx} cy={cy} r={r} color="#c69">
-                      <Paint
-                        color="#fff"
-                        style="stroke"
-                        strokeWidth={strokeWidth}
-                      />
+                    <Circle
+                      cx={cx}
+                      cy={cy}
+                      r={isCurrentDay ? r - strokeWidth / 2 : r}
+                      color="#c69">
+                      {isCurrentDay && (
+                        <Paint
+                          color="#fff"
+                          style="stroke"
+                          strokeWidth={strokeWidth}
+                        />
+                      )}
                       <LinearGradient
                         start={vec(128, 0)}
                         end={vec(128, 100)}
                         colors={['#e336eb', '#e336eb', '#f66938', '#f66938']}
                       />
                     </Circle>
-                    <Path
-                      path={`
-                        M ${cx - r / 2} ${cy}
-                        L ${cx - r / 5} ${cy + r / 3}
-                        L ${cx + r / 2.3} ${cy - r / 2.5}
-                        `}
-                      color="#fff"
-                      style="stroke"
-                      strokeJoin="round"
-                      strokeWidth={3}
-                      strokeCap="round"
-                    />
+                    {fullStreak ? (
+                      <Path
+                        path="m 5.561319,21.272652 c -6.645634,-2.56579 -7.681363,-9.8917 -1.219681,-13.775207 0.233484,2.646707 0.618814,4.426447 2.905708,4.771107 C 10.491127,8.308732 7.599323,3.817087 5.633062,0 c 3.204749,1.494304 8.9952,3.846726 11.04887,9.542202 2.17277,8.09945 -3.82566,11.43731 -4.986342,11.29998 -0.384116,0.12829 5.400152,-4.55778 -0.143492,-10.83363 -0.03587,4.01777 -2.306228,7.27501 -3.085073,7.35395 -1.968685,0.19954 -4.519989,-2.90571 -4.519989,-2.90571 -0.372243,1.3565 -0.278555,4.14182 1.614283,6.81586 z"
+                        color="#fff"
+                        transform={[
+                          { translateX: cx - 9 },
+                          { translateY: cy - 11 },
+                        ]}
+                      />
+                    ) : (
+                      <Path
+                        path={`
+                          M ${cx - r / 2} ${cy}
+                          L ${cx - r / 5} ${cy + r / 3}
+                          L ${cx + r / 2.3} ${cy - r / 2.5}
+                          `}
+                        color="#fff"
+                        style="stroke"
+                        strokeJoin="round"
+                        strokeWidth={3}
+                        strokeCap="round"
+                      />
+                    )}
                   </>
                 ) : (
                   <Circle
